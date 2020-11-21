1905d77b8874471c0352e014f19b6ef7d117618d
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-22 01:22:22 +0900

    Learning prop-types

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const list = [
   {
+    id: 1,
     name: "1",
     img:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TMmeHZc2BO3lzL3tnLQjfgHaE8%26pid%3DApi&f=1",
   },
   {
+    id: 2,
     name: "2",
     img:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.atChPpvEjiXwckGgZypOBAHaEc%26pid%3DApi&f=1",
   },
   {
+    id: 3,
     name: "3",
     img:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OUA9d_lOAtIrOXbbJitryQHaFF%26pid%3DApi&f=1",
   },
   {
+    id: 4,
     name: "4",
     img:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.e6d2nMN8uvbUBFQ-djaoRwHaE7%26pid%3DApi&f=1",
   },
   {
+    id: 5,
     name: "5",
     img:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0LxXqpEEMDwcBWsdgi4Q7wHaEK%26pid%3DApi&f=1",
@@ -30,19 +36,25 @@ const list = [
 
 function Food({ name, picture }) {
   return (
-      <div>
-        <h2> I like {name} </h2>
-        <img src={picture}/>
-      </div>
-    );
+    <div>
+      <h2> 고기나먹어 제껴버리고싶다 시발 {name} </h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+// PropTypes 추가
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+};
 
 function App() {
   return (
     <div>
-      {
-        list.map(dish => (<Food name={dish.name} picture={dish.img} />))
-      }
+      {list.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.img} />
+      ))}
     </div>
   );
 }
