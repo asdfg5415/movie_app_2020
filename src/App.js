70cd5e77adbebb7bf2e81d1d88ddbe404d7d16d2
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-22 00:25:43 +0900

    Learning Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from "react"
+import React from "react";
+
+const list = [
+  {
+    name: "1",
+    img:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.TMmeHZc2BO3lzL3tnLQjfgHaE8%26pid%3DApi&f=1",
+  },
+  {
+    name: "2",
+    img:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.atChPpvEjiXwckGgZypOBAHaEc%26pid%3DApi&f=1",
+  },
+  {
+    name: "3",
+    img:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.OUA9d_lOAtIrOXbbJitryQHaFF%26pid%3DApi&f=1",
+  },
+  {
+    name: "4",
+    img:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.e6d2nMN8uvbUBFQ-djaoRwHaE7%26pid%3DApi&f=1",
+  },
+  {
+    name: "5",
+    img:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.0LxXqpEEMDwcBWsdgi4Q7wHaEK%26pid%3DApi&f=1",
+  },
+];
+
+function Food({ name, picture }) {
+  return (
+      <div>
+        <h2> I like {name} </h2>
+        <img src={picture}/>
+      </div>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        list.map(dish => (<Food name={dish.name} picture={dish.img} />))
+      }
     </div>
   );
 }
