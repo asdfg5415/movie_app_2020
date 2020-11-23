897de03b5b88cc673ed96bb1f0d4910b4a9d4265
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-24 01:43:47 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,55 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
   };
 
-  constructor(props) {
-    super(props);
-    console.log("I'm started!");
-  }
-
-  componentDidMount = () => console.log("I'm Mounted!");
-
-  componentDidUpdate = () => console.log("I'm Updated");
-
-  componentWillUnmount = () => console.log("I'm gonna die");
-
-  // 이렇게 클래스 내의 함수에서 쌩으로 state를 건들면 작동 X
-  add_wrong = () => {
-    this.state.count += 1;
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
 
-  // 이렇게 setState를 통해서 하면 작동할 것처럼 보이지만 X
-  minus_wrong = () => {
-    this.setState({ count: this.count - 1 });
-  };
-
-  // 이렇게 setState를 통해서
-  // 직접적으로 this.state를 건들지않으면서 해야 작동 O
-  add_correct = () => {
-    console.log("Add Button Pressed");
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus_correct = () => {
-    console.log("MINUS Button Pressed");
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
-  };
+  componentDidMount = () => this.getMovies();
 
   render() {
-    console.log("I'm rendering!");
+    const { isLoading } = this.state;
     return (
       <div>
-        <h2>I wanted to be a good boy... my energy is {this.state.count} </h2>
-        <button onClick={this.add_correct}>ADD</button>
-        <button onClick={this.minus_correct}>MINUS</button>
+        <h1> {isLoading ? "Loading..." : "We are Ready!!"} </h1>
       </div>
     );
   }
