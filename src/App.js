1b315fd52b2161217a27b27e1fc94d1355346595
Sanghyeon Shin <asdfg5415@gmail.com>
2020-11-24 00:58:46 +0900

    component LifeCycle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ class App extends React.Component {
     count: 0,
   };
 
+  constructor(props) {
+    super(props);
+    console.log("I'm started!");
+  }
+
+  componentDidMount = () => console.log("I'm Mounted!");
+
+  componentDidUpdate = () => console.log("I'm Updated");
+
+  componentWillUnmount = () => console.log("I'm gonna die");
+
   // 이렇게 클래스 내의 함수에서 쌩으로 state를 건들면 작동 X
   add_wrong = () => {
     this.state.count += 1;
@@ -19,18 +30,21 @@ class App extends React.Component {
   // 이렇게 setState를 통해서
   // 직접적으로 this.state를 건들지않으면서 해야 작동 O
   add_correct = () => {
+    console.log("Add Button Pressed");
     this.setState((current) => ({
       count: current.count + 1,
     }));
   };
 
   minus_correct = () => {
+    console.log("MINUS Button Pressed");
     this.setState((current) => ({
       count: current.count - 1,
     }));
   };
 
   render() {
+    console.log("I'm rendering!");
     return (
       <div>
         <h2>I wanted to be a good boy... my energy is {this.state.count} </h2>
